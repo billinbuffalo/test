ee4605629b938eb493e99376ac15dbb76707cd34
bill <billsiegler@gmail.com>
2016-11-15 19:28:58 -0500

    new

--- visualizations/visualizations.py
+++ visualizations/visualizations.py
@@ -10,23 +10,25 @@ adding a shadow, and changing the starting angle.
 #from pylab import *
 from matplotlib.pylab import *
 
-# make a square figure and axes
-figure(1, figsize=(6,6))
-ax = axes([0.1, 0.1, 0.8, 0.8])
+def piechart(fracs, lables):
+    # make a square figure and axes
+    figure(1, figsize=(6,6))
+    ax = axes([0.1, 0.1, 0.8, 0.8])
 
-# The slices will be ordered and plotted counter-clockwise.
-labels = 'Frogs', 'Hogs', 'Dogs', 'Logs'
-fracs = [15, 30, 45, 10]
-explode=(0, 0.05, 0, 0)
+    # The slices will be ordered and plotted counter-clockwise.
+    #labels = 'Frogs', 'Hogs', 'Dogs', 'Logs'
+    #fracs = [15, 30, 45, 10]
+    explode=(0, 0.05, 0, 0)
 
-pie(fracs, explode=explode, labels=labels,
-                autopct='%1.1f%%', shadow=True, startangle=90)
-                # The default startangle is 0, which would start
-                # the Frogs slice on the x-axis.  With startangle=90,
-                # everything is rotated counter-clockwise by 90 degrees,
-                # so the plotting starts on the positive y-axis.
+    pie(fracs, explode=explode, labels=labels,
+                    autopct='%1.1f%%', shadow=True, startangle=90)
+                    # The default startangle is 0, which would start
+                    # the Frogs slice on the x-axis.  With startangle=90,
+                    # everything is rotated counter-clockwise by 90 degrees,
+                    # so the plotting starts on the positive y-axis.
 
-title('Raining Hogs and Dogs', bbox={'facecolor':'0.8', 'pad':5})
+    title('Raining Hogs and Dogs', bbox={'facecolor':'0.8', 'pad':5})
 
-show()
+    show()
 
+#piechart()
